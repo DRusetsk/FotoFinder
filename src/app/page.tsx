bcde6b1eeb21c07df68e7824ae90f5bff0d87b4a
Alireza Hakim <134637828+alihakimbaba@users.noreply.github.com>
2025-03-09 14:59:02 -0400

    Created components folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import Image from "next/image";
-import Map from './Map';
+import Map from './components/Map';
 import './App.css'
 import 'leaflet/dist/leaflet.css';
 
