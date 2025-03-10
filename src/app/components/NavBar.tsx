7df77a9399a140a02d12d05518293cf77e582d44
johnson4500 <johnsonhan6500@gmail.com>
2025-03-10 03:00:13 -0400

    Project Restructring

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,4 +1,30 @@
+'use client';
+import { useEffect, useState } from "react";
+import { onAuthStateChanged, signOut, User } from "firebase/auth";
+import { auth } from '../firebaseconfig';
+
 export default function NavBar() {
+    const [authorized, setAuthorized] = useState<User>();
+    useEffect(() => {
+        const listen = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setAuthorized(user);
+                console.log(user);
+            }
+
+            return () => {
+                listen();
+            }
+        })
+    }, [onAuthStateChanged]);
+
+    const logout = () => {
+        signOut(auth).then(() => {
+            console.log('Sign out successful.')
+            setAuthorized(undefined);
+        });
+    }
+
     return (
         <div>
             <div id="header-nav">
@@ -12,13 +38,26 @@ export default function NavBar() {
                         <span className="bar3"></span>
                     </a>
 
-                    <nav id="menu" role="navigation">
-                        <ul>
-                            <li><a href="#"></a>Signup</li>
-                            <li><a></a></li>
-                            <li><a href="#"></a>Login</li>
-                        </ul>
-                    </nav>
+                    {authorized === undefined ? (
+                        <nav id="menu" role="navigation">
+                            <ul>
+                                <li><a href="/Signup">Signup</a></li>
+                                <li><a></a></li>
+                                <li><a href="/Login">Login</a></li>
+                            </ul>
+                        </nav>
+                    ) : (
+                        <nav id="menu" role="navigation">
+                            <ul>
+                                <li><a href = "/Submit">Submit</a></li>
+                                <li><a></a></li>
+                                <li><a>Signed in as {authorized.displayName}</a></li>
+                                <li><a></a></li>
+                                <li><a href = "/" onClick={logout}>Logout</a></li>
+                            </ul>
+                        </nav>
+                    )}
+                    
                 </div>
             </div>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,45 @@
 'use client';
-import React from 'react';
-import Image from "next/image";
+import React, { useEffect, useState } from 'react';
 import Map from './components/Map';
-import './App.css'
+import NavBar from './components/NavBar';
+import './assets/App.css'
 import 'leaflet/dist/leaflet.css';
+import MainContainer from './components/MainContainer';
+import { ref as dbRef, onValue } from 'firebase/database';
+import { rtDB } from './firebaseconfig';
 
-export default function Home() {
-  return (
-    <div>
-      <Map/>
-    </div>
-  );
-}
+
+  export default function Home() {
+    interface Photo {
+      imgURLs: {
+          [key: string]: string;
+      };
+      lat: number;
+      long: number;
+      photoDescription: string;
+      photoID: string;
+    }
+
+    const databaseRef = dbRef(rtDB, 'photos/');
+    const [photoCollection, setPhotoCollection] = useState<Photo[]>([]);
+
+    useEffect(() => {
+        onValue(databaseRef, (snapshot) => {
+            const newData: Photo[] = [];
+            snapshot.forEach(childSnapShot => {
+            newData.push(childSnapShot.val());
+        })
+
+        setPhotoCollection(newData);
+    })
+    }, []);
+
+
+    return (
+      <div>
+        <NavBar/>
+        <MainContainer photoCollection={photoCollection}/>
+        <Map photoCollection={photoCollection}/>
+      </div>
+    );
+  }
