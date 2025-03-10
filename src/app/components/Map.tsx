5d1a6a65c1cc49fc79f2260a1aa04e27558980b5
johnson4500 <johnsonhan6500@gmail.com>
2025-03-09 20:09:34 -0400

    Change map theme

--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -1,21 +1,26 @@
 'use client';
 import React from 'react';
 import { MapContainer, TileLayer, Marker, useMap, Popup } from 'react-leaflet';
-import * as leaflet from 'leaflet'
+import {Icon} from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 export default function Map() {
+    const customIcon = new Icon({
+        iconUrl: "https://i.ibb.co/gbMZzB0z/marker-Icon.png",
+        iconSize: [50, 50]
+    });
+
     return (
         <div className = "map-container">
             <MapContainer id = "map" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
                 <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+                    url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
                 />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
+                <Marker icon = {customIcon} position={[51.505, -0.09]}>
+                    {/* <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
+                    </Popup> */}
                 </Marker>
             </MapContainer>
         </div>
