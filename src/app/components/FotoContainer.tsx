79ef9c464be218c6879426aebba52caf2c009fdb
Alireza Hakim <134637828+alihakimbaba@users.noreply.github.com>
2025-03-31 00:19:51 -0400

    Modifying FotoContainer props

--- src/app/components/FotoContainer.tsx
+++ src/app/components/FotoContainer.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 interface FotoContainerProps {
   setPhotoIsClicked: React.Dispatch<React.SetStateAction<boolean>>;
+  setLocationID: React.Dispatch<React.SetStateAction<number>>;
+  locationID: number;
   children: React.ReactNode;
 }
 
 
-const FotoContainer: React.FC<FotoContainerProps> = ({ setPhotoIsClicked, children }) => {
+const FotoContainer: React.FC<FotoContainerProps> = ({ setPhotoIsClicked, setLocationID, locationID, children }) => {
   return <div onClick = {() => {
     setPhotoIsClicked(true);
+    setLocationID(locationID);
+    console.log(locationID);
   }} className="foto-container">{children}</div>;
 };
 
