2c8a11eb5f6bfb7e9d66811965c181fa01225d28
johnson4500 <johnsonhan6500@gmail.com>
2025-03-31 00:25:57 -0400

    Link db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,16 +14,34 @@ import Carousel from './components/Carousel';
   export default function Home() {
     interface Photo {
       imgURLs: {
-          [key: string]: string;
+          [key: string]: {url: string, metadata: {
+            aperture: {
+              denominator: number;
+              numerator: number;
+            }
+            exposureTime: {
+              denominator: number;
+              numerator: number;
+            }
+            focalLength: {
+              denominator: number;
+              numerator: number;
+            }
+            iso: number;
+            lensModel: string;
+            model: string;
+            timestamp: string;
+          }}
       };
       lat: number;
-      long: number;
-      photoDescription: string;
-      photoID: string;
+      lng: number;
+      locationDescription: string;
+      locationName: string;
+      locationAddress: string;
     };
     
 
-    const databaseRef = dbRef(rtDB, 'photos/');
+    const databaseRef = dbRef(rtDB, 'locations/');
     const [photoCollection, setPhotoCollection] = useState<Photo[]>([]);
     const [photoIsClicked, setPhotoIsClicked] = useState<boolean>(false);
     const [locationID, setLocationID] = useState<number>(0);
@@ -41,6 +59,7 @@ import Carousel from './components/Carousel';
 
     const handleMarkerClick = (lID: number) => {
       setLocationID(lID);
+      setPhotoIsClicked(true);
       console.log(lID);
     }
 
@@ -48,19 +67,24 @@ import Carousel from './components/Carousel';
     return (
       <div>
         <NavBar/>
-        <MainContainer photoCollection={photoCollection} setPhotoIsClicked = {setPhotoIsClicked}/>
+        <MainContainer setLocationID = {setLocationID} photoCollection={photoCollection} setPhotoIsClicked = {setPhotoIsClicked}/>
         {photoIsClicked ? (
             <div className = "photo-info-container">
-              <Carousel />
-              <h1 id='photo-title' className='text-white text-2xl font-medium'>Lakeside</h1>
-              <h2 id='photo-desc' className='text-white text-lg'>This is an example description of the lakeside photo</h2>
+              <Carousel locationID = {locationID} photoCollection = {photoCollection} />
+              <h1 id='photo-title' className='text-white text-2xl font-medium'>Location Name: {photoCollection[locationID].locationName}</h1>
+              <h2 id='photo-desc' className='text-white text-lg'>Description: {photoCollection[locationID].locationDescription}</h2>
+              <h2 id='photo-desc' className='text-white text-m'>Address: {photoCollection[locationID].locationAddress}</h2>
+
               <b></b>
-              <h3 id='phot-exif' className='text-white font-light mt-2'>Lens: EF 50mm f1/8, iso:200, Shutter: 1/1000</h3>
+              <h3 id='phot-exif' className='text-white font-light mt-2'>Camera: {photoCollection[locationID].imgURLs[0].metadata.model} Focal Length: {photoCollection[locationID].imgURLs[0].metadata.focalLength.numerator}mm</h3>
+              <h3 id='phot-exif' className='text-white font-light mt-2'>Aperture: f{photoCollection[locationID].imgURLs[0].metadata.aperture.numerator / photoCollection[locationID].imgURLs[0].metadata.aperture.denominator} Shutter Speed: {photoCollection[locationID].imgURLs[0].metadata.exposureTime.numerator}/{photoCollection[locationID].imgURLs[0].metadata.exposureTime.denominator} ISO:{photoCollection[locationID].imgURLs[0].metadata.iso}</h3>      
+              <h3 id='phot-exif' className='text-white font-light mt-2'>Timestamp: {photoCollection[locationID].imgURLs[0].metadata.timestamp}</h3>
+
             </div>
         ) : (
           <></>
         )}
-        <Map photoCollection={photoCollection} handleMarkerClick={handleMarkerClick}/>
+        <Map photoCollection = {photoCollection} handleMarkerClick={handleMarkerClick}/>
       </div>
     );
   }
