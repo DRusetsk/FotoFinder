86a04ef5d6d6fce7f7ee066a4365c1a03363c4a5
Daniel Rusetski <dukarus007@gmail.com>
2025-03-31 09:46:14 -0400

    Update NavBar.tsx

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -35,10 +35,10 @@ export default function NavBar() {
 
     return (
         <div>
-            <div id="header-nav" className="fixed top-0 bg-black z-100 block w-full pb-2.5 h-50">
+            <div id="header-nav" className="fixed top-0 bg-black z-100 block w-full pb-2.5 ">
                 <div className="container">
                     <div className="brand flex justify-between items-center">
-                        <a href="/" className="fixed text-3xl ml-2rem mt-5">FotoFinder</a>
+                        <a href="/" className="fixed text-3xl ml-44 mt-5">FotoFinder</a>
                     </div>
                     <a href="#menu" className="menu-link">
                         <span className="bar1"></span>
