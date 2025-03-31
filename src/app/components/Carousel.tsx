71259606da5b7eaf7ba45980b6edcbd42657d2fe
Daniel <dukarus007@gmail.com>
2025-03-31 00:19:36 -0400

    Update Carousel.tsx

--- src/app/components/Carousel.tsx
+++ src/app/components/Carousel.tsx
@@ -12,37 +12,56 @@ type ImageData = {
   alt: string;
 };
 
-const Carousel = () => {
-  const [images, setImages] = useState<ImageData[]>([]);
-
-  useEffect(() => {
-    const fetchImages = async () => {
-      const res = await fetch("/images.json");
-      const data: ImageData[] = await res.json();
-      setImages(data);
+interface CarouselProps {
+  photoCollection: {
+    imgURLs: {
+      [key: string]: {url: string, metadata: {
+          aperture: {
+            denominator: number;
+            numerator: number;
+          }
+          exposureTime: {
+            denominator: number;
+            numerator: number;
+          }
+          focalLength: {
+            denominator: number;
+            numerator: number;
+          }
+          iso: number;
+          lensModel: string;
+          model: string;
+          timestamp: string;
+        }}
     };
+    lat: number;
+    lng: number;
+    locationDescription: string;
+    locationName: string;
+  }[];
 
-    fetchImages();
-  }, []);
+  locationID: number;
+}
 
+const Carousel: React.FC<CarouselProps> = ({ photoCollection, locationID }) => {
   return (
-    <Swiper
-      modules={[Navigation, Pagination, Autoplay]}
-      spaceBetween={20}
-      slidesPerView={1}
-      navigation
-      pagination={{ clickable: true }}
-      autoplay={{ delay: 3000 }}
-      loop
-    >
-      {images.map((image) => (
-        <SwiperSlide key={image.id}>
-          <div className="flex justify-center">
-            <Image src={image.url} alt={image.alt} width={800} height={500} className="rounded-lg" />
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+      <Swiper
+          modules={[Navigation, Pagination, Autoplay]}
+          spaceBetween={20}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true }}
+          autoplay={{ delay: 3000 }}
+          loop
+      >
+        {Object.entries(photoCollection[locationID].imgURLs).map(([key, value]) => (
+            <SwiperSlide key={key}>
+              <div className="flex justify-center">
+                <Image src={value.url} alt={value.url} width={800} height={500} className="rounded-lg" />
+              </div>
+            </SwiperSlide>
+        ))}
+      </Swiper>
   );
 };
 
