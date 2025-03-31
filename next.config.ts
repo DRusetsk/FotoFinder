0c185c42dbf2c2d7a15c2bee67008173c0ba9e0a
Alireza Hakim <134637828+alihakimbaba@users.noreply.github.com>
2025-03-31 00:16:44 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images:{
-    domains:["lassonde.yorku.ca", "live.staticflickr.com", "attractionsontario.ca"]
+    domains:["lassonde.yorku.ca", "live.staticflickr.com", "attractionsontario.ca", "firebasestorage.googleapis.com"]
   }
 };
 
