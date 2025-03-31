0bf7e60334531a2dd1c37c7bd402681fae5b5301
Daniel <dukarus007@gmail.com>
2025-03-31 01:04:48 -0400

    Created Close Button for info container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,10 @@ import Carousel from './components/Carousel';
       console.log(lID);
     }
 
-
+    const closeWindow = () =>{
+      setPhotoIsClicked(false)
+    }
+    <button  className="mt-5 text-white"onClick={closeWindow}>Close</button>
     return (
       <div>
         <NavBar/>
@@ -73,13 +76,13 @@ import Carousel from './components/Carousel';
               <Carousel locationID = {locationID} photoCollection = {photoCollection} />
               <h1 id='photo-title' className='text-white text-2xl font-medium'>Location Name: {photoCollection[locationID].locationName}</h1>
               <h2 id='photo-desc' className='text-white text-lg'>Description: {photoCollection[locationID].locationDescription}</h2>
-              <h2 id='photo-desc' className='text-white text-m'>Address: {photoCollection[locationID].locationAddress}</h2>
+              <h2 id='photo-desc' className='text-white text-m'>Location: {photoCollection[locationID].locationAddress}</h2>
 
               <b></b>
               <h3 id='phot-exif' className='text-white font-light mt-2'>Camera: {photoCollection[locationID].imgURLs[0].metadata.model} Focal Length: {photoCollection[locationID].imgURLs[0].metadata.focalLength.numerator}mm</h3>
-              <h3 id='phot-exif' className='text-white font-light mt-2'>Aperture: f{photoCollection[locationID].imgURLs[0].metadata.aperture.numerator / photoCollection[locationID].imgURLs[0].metadata.aperture.denominator} Shutter Speed: {photoCollection[locationID].imgURLs[0].metadata.exposureTime.numerator}/{photoCollection[locationID].imgURLs[0].metadata.exposureTime.denominator} ISO:{photoCollection[locationID].imgURLs[0].metadata.iso}</h3>      
-              <h3 id='phot-exif' className='text-white font-light mt-2'>Timestamp: {photoCollection[locationID].imgURLs[0].metadata.timestamp}</h3>
-
+              <h3 id='phot-exif' className='text-white font-light'>Aperture: f{photoCollection[locationID].imgURLs[0].metadata.aperture.numerator / photoCollection[locationID].imgURLs[0].metadata.aperture.denominator} Shutter Speed: {photoCollection[locationID].imgURLs[0].metadata.exposureTime.numerator}/{photoCollection[locationID].imgURLs[0].metadata.exposureTime.denominator} ISO:{photoCollection[locationID].imgURLs[0].metadata.iso}</h3>      
+              <h3 id='phot-exif' className='text-white font-light'>Timestamp: {photoCollection[locationID].imgURLs[0].metadata.timestamp}</h3>
+              <button variant="secondary" className="mt-5 text-white" onClick={closeWindow}>Close</button>
             </div>
         ) : (
           <></>
